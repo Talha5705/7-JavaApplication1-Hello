51a5b8082e28e18a43e77372da3ce31aa5d14668
Talha5705 <ztalha@gmail.com>
2018-10-12 21:18:53 -0700

    first commit

---
+++ src/javaapplication1/hello/JavaApplication1Hello.java
@@ -0,0 +1,12 @@
+
+package javaapplication1.hello;
+
+
+public class JavaApplication1Hello {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello dude");
+    }
+    
+}
